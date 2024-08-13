b71fd3f97409205acce3c24dbb95047443ada3c1
DaianeMGouvea <daianegouvea.dev@gmail.com>
2024-08-13 18:55:23 -0300

    config: add RSpec for testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with valid attributes" do
+    user = User.new(email: "test@example.com", password: "password123")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without an email" do
+    user = User.new(password: "password123")
+    expect(user).not_to be_valid
+  end
+
+  it "is not valid without a password" do
+    user = User.new(email: "test@example.com")
+    expect(user).not_to be_valid
+  end
+end
