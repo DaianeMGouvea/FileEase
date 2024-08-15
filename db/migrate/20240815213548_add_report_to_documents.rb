87b8b7d7af5c71147aa3068e96c59147bf77f897
DaianeMGouvea <daianegouvea.dev@gmail.com>
2024-08-15 19:33:38 -0300

    feat: XML reading extraction and formatting

---
+++ db/migrate/20240815213548_add_report_to_documents.rb
@@ -0,0 +1,5 @@
+class AddReportToDocuments < ActiveRecord::Migration[7.0]
+  def change
+    add_column :documents, :report, :jsonb
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_13_220010) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_15_213548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_13_220010) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "report"
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
